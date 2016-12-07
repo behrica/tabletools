7bb5b9dad46633705aac0446551b63047a0c2009
Carsten Behring <carsten.behring@gmail.com>
2016-12-07 17:57:54 +0100

    Initial commit

---
+++ R/tabletools.R
@@ -0,0 +1,31 @@
+#' Converts any html into a png file
+#' @param html HTML text to convert
+#' @param zoom Zoomfactor
+#' @param rotate Rotation 0 - 360
+#' @return A file name of the temporary png file created
+#' @export
+htmlToPng <- function(html,zoom = 1,rotate = 0,vwidth = 992,vheight = 74,cliprect=NULL) {
+    htmlFile <- tempfile(fileext = ".html")
+    pngFile <- tempfile(fileext = ".png")
+    writeLines(html,htmlFile)
+    webshot::webshot(htmlFile,pngFile,zoom=zoom,vwidth = vwidth,vheight = vheight,cliprect = cliprect)
+    if (rotate!=0) {
+        magick::image_read(pngFile) %>%
+            magick::image_rotate(270) %>%
+                magick::image_write(pngFile)
+    }
+    pngFile
+
+}
+
+##' Converts a FlexTable into a png file
+#' @param ft The FlexTable object to convert
+#' @param zoom Zoomm level
+#' @param rotate Rotation 0 - 360
+#' @importFrom magrittr %>%
+#' @return Path of png file created
+#' @export
+ftToPng <- function(ft,zoom = 1,rotate=0,vwidth = 992,vheight = 744,cliprect=0) {
+    ft %>% ReporteRs::as.html() %>%
+        htmlToPng(zoom,rotate,vwidth,vheight,cliprect)
+}
